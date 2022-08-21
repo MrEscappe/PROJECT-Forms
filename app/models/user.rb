8e9143172be4673b098302c7ed780f6588bb4780
José Sérgio <sergiosilva@outlook.com>
2022-08-21 18:14:20 -0300

    Form creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
+  validates :username, length: { in: 3..12 }, presence: true, iniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { in: 4..18 }
 end
